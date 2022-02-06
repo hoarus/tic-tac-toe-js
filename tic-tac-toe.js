ee4884f1f8ab5c502f3a3c9965e4df5ddc412828
hoarus <hoarus@gmail.com>
2022-02-06 10:02:29 +1000

    Configure functionality to allow clicking on gamesquares to add player token unless square is already occupied

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,5 @@
 
+// Global Variables
 let siteBoard = document.querySelector(".gameBoard");
 let siteRows = document.querySelectorAll(".gameboard-row");
 let siteSquares = document.querySelectorAll('.gamesquare');
@@ -9,42 +10,73 @@ let siteSquares = document.querySelectorAll('.gamesquare');
 
 const gameBoard = (function() {
   let gameSquares = [
-    " ", "X", " ", 
-    " ", "O", " ", 
+    " ", " ", " ", 
+    " ", " ", " ", 
     " ", " ", " "
   ];
   const printGameSquares = () => { 
-    console.log(gameSquares); 
-    console.log(siteSquares.length);
     for (let i = 0; i < siteSquares.length; i++) {
       let square = siteSquares[i];
       square.textContent = gameSquares[i];
     }
   };
 
+  const placeToken = (square, token) => {
+    gameSquares[square] = token;
+  };
 
-  
-  let test="yest";
+  const isSquareEmpty = (square) => {
+    return gameSquares[square] === " ";
+  };
+
+
   return {
     printGameSquares,
-    test
+    placeToken,
+    isSquareEmpty,
   };
 
 })(); 
 
 
-const player = (name) => {
+const player = (name, token) => {
   return {
-    name
+    name,
+    token,
   }
 }
 
 
+// Gameflow
+
 const displayController = (() => {
-  const playerOne = player("Player One");
-  const playerTwo = player("player Two");
+  const playerOne = player("Player One", "X");
+  const playerTwo = player("player Two", "O");
+  let currentPlayer = playerOne;
+
+  function alternatePlayer() {
+    if (currentPlayer === playerOne) {
+      currentPlayer = playerTwo;
+    } else {
+      currentPlayer = playerOne;
+    }
+  }
   console.log(playerOne.name);
+  console.log(playerOne.token);
   gameBoard.printGameSquares();
+
+  for (const siteSquare of siteSquares ) {
+  siteSquare.addEventListener('click', function(event) {
+    let squareId = siteSquare.id;
+    let squareNumber = parseInt(squareId.charAt(squareId.length -1));
+    // NEED TO CHECK IF SQUARE IS FULL (LOGIC SHOULD BE IN GAMEBOARD)
+    if (gameBoard.isSquareEmpty(squareNumber)) {
+      gameBoard.placeToken(squareNumber, currentPlayer.token);
+      gameBoard.printGameSquares();
+      alternatePlayer();
+    }
+  })
+}
 })();
 
 
