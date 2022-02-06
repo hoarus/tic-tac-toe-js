a5706342a2886d91f60373214818a8b975f63d55
hoarus <hoarus@gmail.com>
2022-02-06 15:36:46 +1000

    configure game over logic and reset game button

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,9 +3,8 @@
 let siteBoard = document.querySelector(".gameBoard");
 let siteRows = document.querySelectorAll(".gameboard-row");
 let siteSquares = document.querySelectorAll('.gamesquare');
-
-
-
+let instructions = document.querySelector(".instructions");
+let playAgainButton = document.querySelector('.play-again');
 
 
 const gameBoard = (function() {
@@ -14,6 +13,8 @@ const gameBoard = (function() {
     " ", " ", " ", 
     " ", " ", " "
   ];
+
+
   const printGameSquares = () => { 
     for (let i = 0; i < siteSquares.length; i++) {
       let square = siteSquares[i];
@@ -23,17 +24,53 @@ const gameBoard = (function() {
 
   const placeToken = (square, token) => {
     gameSquares[square] = token;
+    siteSquares[square].classList.remove('active-square');
   };
 
   const isSquareEmpty = (square) => {
     return gameSquares[square] === " ";
   };
 
+  const hasWonGame = (token) => {
+
+    if (victoryCondition(token, 0, 1, 2) == true) {
+      return true;
+    } else if  (victoryCondition(token, 3, 4, 5) == true){
+      return true;
+    } else if  (victoryCondition(token, 6, 7, 8) == true){
+      return true;
+    } else if  (victoryCondition(token, 0, 4, 8) == true){
+      return true;
+    } else if  (victoryCondition(token, 2, 4, 6) == true){
+      return true;
+    } else if  (victoryCondition(token, 0, 3, 6) == true){
+      return true;
+    } else if  (victoryCondition(token, 2, 5, 8) == true){
+      return true;
+    } else
+      return false;
+    };
+
+    const victoryCondition = (token, var1, var2, var3) => {
+      return  gameSquares[var1] === token && gameSquares[var2] === token && gameSquares[var3] === token;
+    }
+  
+    const resetGameBoard = () => {
+      gameSquares = [
+        " ", " ", " ", 
+        " ", " ", " ", 
+        " ", " ", " "
+      ];
+      siteSquares.forEach(square => square.classList.add('active-square'));
+      printGameSquares();
+    }
 
   return {
     printGameSquares,
     placeToken,
     isSquareEmpty,
+    hasWonGame,
+    resetGameBoard,
   };
 
 })(); 
@@ -51,30 +88,59 @@ const player = (name, token) => {
 
 const displayController = (() => {
   const playerOne = player("Player One", "X");
-  const playerTwo = player("player Two", "O");
+  const playerTwo = player("Player Two", "O");
   let currentPlayer = playerOne;
+  let isGameOver = false;
+
+  playAgainButton.addEventListener('click', function(event) {
+    gameBoard.resetGameBoard();
+    playAgainButton.classList.add('hidden');
+    currentPlayer = playerOne;
+    isGameOver = false;
+    instructions.textContent = `${currentPlayer.name}'s turn.`;
+    instructions.classList.remove('instructions-won');
+  });
 
   function alternatePlayer() {
     if (currentPlayer === playerOne) {
       currentPlayer = playerTwo;
+      instructions.textContent = `${currentPlayer.name}'s turn.`
     } else {
       currentPlayer = playerOne;
+      instructions.textContent = `${currentPlayer.name}'s turn.`
     }
-  }
-  console.log(playerOne.name);
-  console.log(playerOne.token);
+  };
+
+  function hasPlayerWon() {
+    if (gameBoard.hasWonGame(currentPlayer.token)) {
+      isGameOver = true;
+      let message = `${currentPlayer.name} has won the game. Congratulations!`;
+      instructions.textContent = message;
+      instructions.classList.add("instructions-won");
+      siteSquares.forEach(square => square.classList.remove("active-square"));
+      playAgainButton.classList.remove("hidden");
+    }
+  };
+
+  function playRound(squareNumber){
+    if (gameBoard.isSquareEmpty(squareNumber)) {
+      gameBoard.placeToken(squareNumber, currentPlayer.token);
+      gameBoard.printGameSquares();
+      hasPlayerWon();
+      alternatePlayer();  
+    }
+    
+  };
   gameBoard.printGameSquares();
 
   for (const siteSquare of siteSquares ) {
   siteSquare.addEventListener('click', function(event) {
+    
     let squareId = siteSquare.id;
     let squareNumber = parseInt(squareId.charAt(squareId.length -1));
-    // NEED TO CHECK IF SQUARE IS FULL (LOGIC SHOULD BE IN GAMEBOARD)
-    if (gameBoard.isSquareEmpty(squareNumber)) {
-      gameBoard.placeToken(squareNumber, currentPlayer.token);
-      gameBoard.printGameSquares();
-      alternatePlayer();
-    }
+    if (isGameOver === false) {
+      playRound(squareNumber);
+    };
   })
 }
 })();
